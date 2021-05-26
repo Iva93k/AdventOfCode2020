52210bf4029b516bb2113f9d8b0a45dd1ec802c2
IVANA KOVAČEVIĆ <iva93k@gmail.com>
2021-05-26 15:31:20 +0200

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputFile = File.ReadAllLines("C:/AdventOfCode2020/Day6/Day6Input.txt");
+            Console.WriteLine($"Solution for the first part: {DaySixPartOne(inputFile)}");
+
+        }
+
+        private static int DaySixPartOne(string[] inputFile)
+        {
+            int sum = 0;
+            List<string> allAnswers = new List<string>();
+            string oneGroupAnswers = String.Empty;
+
+
+            foreach (var input in inputFile)
+            {
+
+                if (input == String.Empty)
+                {
+                    allAnswers.Add(oneGroupAnswers);
+                    oneGroupAnswers = string.Empty;
+                    continue;
+                }
+
+                oneGroupAnswers = oneGroupAnswers + input;
+
+            }
+            allAnswers.Add(oneGroupAnswers);
+
+            foreach (string answerFromoneGroup in allAnswers)
+            {
+                int count = answerFromoneGroup.Distinct().Count();
+                sum += count;
+            }
+
+            return sum;
+        }
+    }
+}
